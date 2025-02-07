fdefbcec29cc69d64d925c597bfbd26fe12e7315
JungHun98 <j7papa@naver.com>
2025-02-07 16:06:33 +0900

    refactor: 불필요한 주석 삭제

--- src/components/MainContent/MainContent.tsx
+++ src/components/MainContent/MainContent.tsx
@@ -3,27 +3,18 @@ import SolutionContainer from '@/components/SolutionContainer';
 import { Wrapper } from './MainContent.styles';
 import { useProblemNumber } from '@/store/store';
 import HorizonResizingBox from '@/components/common/ResizingBox/HorizonResizingBox';
-// import { Mobile, PC } from '@/components/common/MediaQurey/MediaQurey';
 
 function MainContent() {
   const problemNumber = useProblemNumber();
 
   return (
     <>
-      {/* <PC> */}
       <Wrapper>
         <HorizonResizingBox initialLeftRatio={0.4}>
           <ProblemContainer key={problemNumber} problemNumber={problemNumber} />
           <SolutionContainer problemNumber={problemNumber} />
         </HorizonResizingBox>
       </Wrapper>
-      {/* </PC> */}
-      {/* <Mobile>
-        <Wrapper>
-          <ProblemContainer key={problemNumber} problemNumber={problemNumber} />
-          <SolutionContainer problemNumber={problemNumber} />
-        </Wrapper>
-      </Mobile> */}
     </>
   );
 }
